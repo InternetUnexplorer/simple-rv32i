8c9cc2d0c76daf7d4e5e75caa97a8db23d6a6995
InternetUnexplorer <internetunexplorer@gmail.com>
2021-11-27 16:10:16 -0800

    Tweak RegisterFile and Alu slightly

--- src/main/scala/components/Alu.scala
+++ src/main/scala/components/Alu.scala
@@ -1,25 +1,22 @@
 package components
 
-import chisel3.util.MuxLookup
 import chisel3._
 import chisel3.experimental.ChiselEnum
+import chisel3.util._
 
 object AluOp extends ChiselEnum {
-  // Addition and subtraction
-  val ADD = Value
-  val SUB = Value
-
-  // Logic
-  val AND = Value
-  val OR  = Value
-  val XOR = Value
-
-  // Shifts
-  val SLL  = Value
-  val SRL  = Value
-  val SLT  = Value
-  val SLTU = Value
-  val SRA  = Value
+  val ADD   = Value
+  val SUB   = Value
+  val AND   = Value
+  val OR    = Value
+  val XOR   = Value
+  val SLL   = Value
+  val SRL   = Value
+  val SLT   = Value
+  val SLTU  = Value
+  val SRA   = Value
+  val COPYB = Value
+  val Any   = ADD
 }
 
 class AluIO extends Bundle {
@@ -32,21 +29,20 @@ class AluIO extends Bundle {
 class Alu extends Module {
   val io = IO(new AluIO)
 
-  val shiftAmount = io.b(4, 0).asUInt()
+  val shamt = io.b(4, 0).asUInt
 
-  val lookup =
-    Seq(
-      AluOp.ADD.asUInt()  -> (io.a + io.b),
-      AluOp.SUB.asUInt()  -> (io.a - io.b),
-      AluOp.AND.asUInt()  -> (io.a & io.b),
-      AluOp.OR.asUInt()   -> (io.a | io.b),
-      AluOp.XOR.asUInt()  -> (io.a ^ io.b),
-      AluOp.SLL.asUInt()  -> (io.a << shiftAmount),
-      AluOp.SRL.asUInt()  -> (io.a >> shiftAmount),
-      AluOp.SRA.asUInt()  -> (io.a.asSInt() >> shiftAmount).asUInt(),
-      AluOp.SLT.asUInt()  -> (io.a.asSInt() < io.b.asSInt()),
-      AluOp.SLTU.asUInt() -> (io.a < io.b)
-    )
+  val mapping = Seq(
+    AluOp.ADD.asUInt  -> (io.a + io.b),
+    AluOp.SUB.asUInt  -> (io.a - io.b),
+    AluOp.AND.asUInt  -> (io.a & io.b),
+    AluOp.OR.asUInt   -> (io.a | io.b),
+    AluOp.XOR.asUInt  -> (io.a ^ io.b),
+    AluOp.SLL.asUInt  -> (io.a << shamt),
+    AluOp.SRL.asUInt  -> (io.a >> shamt),
+    AluOp.SRA.asUInt  -> (io.a.asSInt >> shamt).asUInt,
+    AluOp.SLT.asUInt  -> (io.a.asSInt < io.b.asSInt),
+    AluOp.SLTU.asUInt -> (io.a < io.b)
+  )
 
-  io.out := MuxLookup(io.op.asUInt(), 0.U, lookup)
+  io.out := MuxLookup(io.op.asUInt, io.b, mapping)
 }
--- src/main/scala/components/RegisterFile.scala
+++ src/main/scala/components/RegisterFile.scala
@@ -8,28 +8,18 @@ class RegisterFileIO extends Bundle {
   val read2Addr = Input(UInt(5.W))
   val read2Data = Output(UInt(32.W))
 
-  val writeAddr   = Input(UInt(5.W))
-  val writeData   = Input(UInt(32.W))
-  val writeEnable = Input(Bool())
+  val writeAddr = Input(UInt(5.W))
+  val writeData = Input(UInt(32.W))
 }
 
 class RegisterFile extends Module {
   val io        = IO(new RegisterFileIO)
   val registers = Mem(32, UInt(32.W))
 
-  io.read1Data := Mux(
-    io.read1Addr.orR(),
-    registers.read(io.read1Addr),
-    0.U
-  )
+  io.read1Data := Mux(io.read1Addr.orR, registers.read(io.read1Addr), 0.U)
+  io.read2Data := Mux(io.read2Addr.orR, registers.read(io.read2Addr), 0.U)
 
-  io.read2Data := Mux(
-    io.read2Addr.orR(),
-    registers.read(io.read2Addr),
-    0.U
-  )
-
-  when(io.writeEnable & io.writeAddr.orR()) {
+  when(io.writeAddr.orR) {
     registers.write(io.writeAddr, io.writeData)
   }
 }
