cbd955e9d1e61b9c053b5f233d471e3f2712db51
InternetUnexplorer <internetunexplorer@gmail.com>
2021-12-04 19:31:09 -0800

    Add some tests for specific instructions

--- src/test/scala/CpuTester.scala
+++ src/test/scala/CpuTester.scala
@@ -17,6 +17,49 @@ class CpuTester extends AnyFlatSpec with ChiselScalatestTester {
     words.map(x => Integer.toUnsignedLong(x).asUInt(32.W))
   }
 
+  def testCpu(program: Seq[UInt], expectedDebugVal: UInt): TestResult = {
+    test(new Cpu(MemoryParams(program, 1024))) { c =>
+      while (c.debugIO.debugVal.peek().litValue == 0)
+        c.clock.step()
+      c.debugIO.debugVal.expect(expectedDebugVal)
+    }
+  }
+
+  it should "handle LUI correctly" in {
+    testCpu(
+      Seq(
+        "h00000513".U, // ADDI a0, zero, 0x0
+        "h00100593".U, // ADDI a1, zero, 0x1
+        "habcde5b7".U, // LUI a1, 0xabcde
+        "h00000073".U  // ECALL
+      ),
+      "habcde000".U
+    )
+  }
+
+  it should "handle AUIPC correctly" in {
+    testCpu(
+      Seq(
+        "h00000513".U, // ADDI a0, zero, 0x0
+        "h12345597".U, // AUIPC a1, 0x12345
+        "h00000073".U  // ECALL
+      ),
+      "h12345004".U
+    )
+  }
+
+  it should "handle JAL correctly" in {
+    testCpu(
+      Seq(
+        "h00000513".U, // ADDI a0, zero, 0x0
+        "h008005ef".U, // JAL a1, 0xc
+        "h00000073".U, // ECALL
+        "hffdff5ef".U  // JAL a1, 0x8
+      ),
+      "h00000010".U
+    )
+  }
+
   it should "run a simple program" in {
     val memoryParams = MemoryParams(loadProgram("/test.bin"), 1024)
 
